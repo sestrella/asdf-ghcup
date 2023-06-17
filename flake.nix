62d73e2f4eb2ee8a0e6317e7bed2044b88ccc28c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-06-17 16:19:54 -0500

    ci: Install semantic-release via Nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
   inputs = {
+    devenv.url = "github:cachix/devenv";
+    nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  outputs = inputs@{ self, devenv, nixpkgs, nixpkgs-master, systems, ... }:
     let
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
@@ -14,22 +15,30 @@
         (system:
           let
             pkgs = nixpkgs.legacyPackages.${system};
+            pkgs-master = nixpkgs-master.legacyPackages.${system};
           in
           {
+            release = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [{
+                packages = [
+                  pkgs-master.semantic-release
+                ];
+              }];
+            };
+
             default = devenv.lib.mkShell {
               inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = [
-                    pkgs.shellcheck
-                  ];
+              modules = [{
+                # https://devenv.sh/reference/options/
+                packages = [
+                  pkgs.shellcheck
+                ];
 
-                  languages.javascript.enable = true;
+                languages.javascript.enable = true;
 
-                  pre-commit.hooks.shellcheck.enable = true;
-                }
-              ];
+                pre-commit.hooks.shellcheck.enable = true;
+              }];
             };
           });
     };
