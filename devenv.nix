8a7939ab93266325168878be55d381ecaaf6403e
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-12-03 15:33:54 -0500

    Reformat code with shfmt

--- devenv.nix
+++ devenv.nix
@@ -1,28 +1,9 @@
 { pkgs, ... }:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  packages = [ pkgs.shfmt ];
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  languages.javascript.enable = true;
 
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
   pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
 }
