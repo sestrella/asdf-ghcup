70a3ba6d244a6b9e095e1410d036723017e15e90
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-12-02 23:28:05 -0500

    Move all logic to utils.bash

---
+++ lib/utils.bash
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+asdf_plugin_path() {
+  echo "$(dirname "$(dirname "$0")")"
+}
+
+ghcup_bin_dir() {
+  echo "$(asdf_plugin_path)/.ghcup/bin"
+}
+
+check_ghcup() {
+  if ! test -f "$(ghcup_bin_dir)/ghcup"
+  then
+    curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | env \
+      BOOTSTRAP_HASKELL_MINIMAL=1 \
+      BOOTSTRAP_HASKELL_NONINTERACTIVE=1 \
+      GHCUP_INSTALL_BASE_PREFIX="$(asdf_plugin_path)" \
+      sh
+  fi
+}
+
+list_all_versions() {
+  check_ghcup
+  "$(ghcup_bin_dir)/ghcup" list -t "$1" -r | awk '{printf $2" "}'
+}
+
+ver() {
+  echo "$1" | awk -F . '{ printf("%02d%02d", $1,$2); }'
+}
+
+install_version() {
+  local toolname="$1"
+  local version="$2"
+  local path="$3"
+
+  check_ghcup
+
+  if [[ "$toolname" == "ghc" ]] || { [[ "$toolname" == "hls"  ]] && [[ $(ver "$version") -ge $(ver "1.7") ]]; }
+  then
+    "$(ghcup_bin_dir)/ghcup" install "$toolname" "$version" -i "$path"
+  else
+    "$(ghcup_bin_dir)/ghcup" install "$toolname" "$version" -i "${path}/bin"
+  fi
+}
