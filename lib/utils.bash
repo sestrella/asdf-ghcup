2ae491ba039c66531f03b4496d040df34a0e81c4
Joaquín Rossi <joaquin@joaquinrossi.net>
2025-10-06 19:51:17 -0300

    Add GHCUP_USE_XDG_DIRS

--- lib/utils.bash
+++ lib/utils.bash
@@ -13,6 +13,7 @@ ensure_ghcup() {
 		curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | env \
 			BOOTSTRAP_HASKELL_MINIMAL=1 \
 			BOOTSTRAP_HASKELL_NONINTERACTIVE=1 \
+			GHCUP_USE_XDG_DIRS=false \
 			GHCUP_INSTALL_BASE_PREFIX="$(asdf_plugin_path)" \
 			sh >/dev/null
 	fi
