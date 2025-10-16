470c6b694b69f201f9c418ecdf1ca7b8f06be87f
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-10-16 00:25:02 -0500

    Full path + no upgrade GHCUP

--- lib/utils.bash
+++ lib/utils.bash
@@ -9,6 +9,7 @@ ensure_ghcup() {
 		curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | env \
 			BOOTSTRAP_HASKELL_MINIMAL=1 \
 			BOOTSTRAP_HASKELL_NONINTERACTIVE=1 \
+			BOOTSTRAP_HASKELL_NO_UPGRADE=1 \
 			GHCUP_INSTALL_BASE_PREFIX="${asdf_plugin_path}" \
 			sh >/dev/null
 	fi
