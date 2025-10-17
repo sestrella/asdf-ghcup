4c2cbcbffa23949f1a03441f299d6bc2d86206a6
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-10-17 12:18:03 -0500

    feat: Add shellcheck and shfmt git hooks
    Adds the `shellcheck` and `shfmt` git hooks to the development environment.
    These hooks will help ensure the quality and formatting of shell scripts.
    
    autocommitmsg(model=gemini-2.5-flash-lite,response_time=1.370774042s,execution_time=1.379609125s)

--- devenv.nix
+++ devenv.nix
@@ -8,10 +8,14 @@
 {
   env.GEMINI_API_KEY = config.secretspec.secrets.GEMINI_API_KEY or "";
 
-  git-hooks.hooks.autocommitmsg = {
-    enable = true;
-    entry = lib.getExe pkgs.autocommitmsg;
-    stages = [ "prepare-commit-msg" ];
-    verbose = true;
+  git-hooks.hooks = {
+    autocommitmsg = {
+      enable = true;
+      entry = lib.getExe pkgs.autocommitmsg;
+      stages = [ "prepare-commit-msg" ];
+      verbose = true;
+    };
+    shellcheck.enable = true;
+    shfmt.enable = true;
   };
 }
