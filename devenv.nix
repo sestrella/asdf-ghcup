5719955e123eb37d9410739b467c5af6947a2ce7
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2026-05-23 21:25:15 -0500

    Update devenv.lock files

--- devenv.nix
+++ devenv.nix
@@ -1,21 +1,13 @@
 {
-  config,
   lib,
   pkgs,
   ...
 }:
 
 {
-  env.GEMINI_API_KEY = config.secretspec.secrets.GEMINI_API_KEY or "";
-
   packages = [ pkgs.asdf-vm ];
 
   git-hooks.hooks = {
-    autocommitmsg = {
-      enable = true;
-      entry = lib.getExe pkgs.autocommitmsg;
-      stages = [ "prepare-commit-msg" ];
-    };
     gitleaks = {
       enable = true;
       entry = lib.getExe pkgs.gitleaks;
